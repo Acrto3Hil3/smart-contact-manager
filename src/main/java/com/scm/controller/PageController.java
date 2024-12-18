15e56574e439d2afd3d01b11769ae3eb529e411c
Acrto3Hil3 <subhashyadav98146@gmail.com>
2024-12-18 23:38:41 +0530

    package.json for tailwind initialization

--- src/main/java/com/scm/controller/PageController.java
+++ src/main/java/com/scm/controller/PageController.java
@@ -1,13 +1,26 @@
 package com.scm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller 
 public class PageController {
-	
+
+	// home page redirection
 	@RequestMapping("/")
-	public String home() {
+	public String homePage() {
 		return "home";
+	}
+	// about page redirection
+	@RequestMapping("/about")
+	public String aboutPage(Model model) {
+		model.addAttribute("isLogin", true);
+		return "about";
+	}
+	// services page redirection
+	@RequestMapping("/services")
+	public String servicesPage() {
+		return "services";
 	}
 }
